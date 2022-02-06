c157a102d3f5e69fbf52ff682a5b201cb6e11947
Carl-CR <accr0214@gmail.com>
2022-02-06 16:43:51 -0500

    edited lab02

--- lab02/app/src/test/java/SimpleTest/AppTest.java
+++ lab02/app/src/test/java/SimpleTest/AppTest.java
@@ -3,6 +3,9 @@
  */
 package SimpleTest;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 class AppTest {
     @Test void appHasAGreeting() {
         App classUnderTest = new App();
